c608d8ec3fcf69d4ee5b19aa99009539062c1766
Παναγιώτης Κόντος <panagiotiskont13@gmail.com>
2020-12-16 16:10:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,23 @@
-var scores = [0,0];
-var roundScore = 0;
-var activeplayer = 0;
-var dice = 0;
-
-// Change the values from an id element using javascript
-document.getElementById("score--0").innerHTML = 0;
-document.getElementById("score--1").innerHTML = 0;
+var scores,activeplayer,dice,roundScore;
+var gamePlaying; //true or false
 
 
-
-// Event Listener for button ROLL DICE
-
-document.querySelector(".btn--roll").addEventListener('click',function(){
-
-    // What happens when you use the button "ROLL DICE"
-
-    
-    // generate random number
-    dice = Math.floor(Math.random()*6+1); //generate a value for dice//
-
-    // display the result
-    var diceDOM =  document.querySelector(".dice");
-    diceDOM.style.display = 'block';
-    /*change src attribut using javascript*/ diceDOM.src = "dice-"+dice+".png";
-    
-    // Upgrade the round score IF the rolled number was not 1
-    if(dice !== 1)
-    {
-        
-       roundScore += dice; // update current score for the active player
-       document.querySelector("#current--"+activeplayer).innerHTML = roundScore;
-
-    }else{
-
-        roundScore = 0;
-        nextPlayer();
-    }
-});
+function initGame()
+{
+    scores = [0,0];
+    activeplayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
 
 
-// Event Listener for button HOLD
+    document.querySelector("#score--0").textContent = scores[0]; //init the total points for each player
+    document.querySelector("#score--1").textContent = scores[1];
+    document.querySelector(".dice").style.display = "none";
 
-document.querySelector(".btn--hold").addEventListener('click',function(){
-    // Add currentScore to Global Score
-    scores[activeplayer] += roundScore;
-    // or more easily scores[activeplayer] += roundScore;
-    // Update UI
-    document.querySelector("#score--"+activeplayer).textContent = scores[activeplayer];
+    document.querySelector("#name--0").innerHTML="PLAYER 1";
+    document.querySelector("#name--1").innerHTML="PLAYER 2";
 
-    
-    //Check if the player won the game
-    if(scores[activeplayer] >= 20)
-    {
-        document.getElementById("name--"+activeplayer).textContent = "WINNER";
-        document.querySelector('.dice').style.display = 'none';
-    
-    }
-        
-    // Next player
-    nextPlayer();
-
-});
-
+}
 
 function nextPlayer()
 {
@@ -84,7 +40,98 @@ function nextPlayer()
 
 
 
+initGame();
+
+
+
+
+
+
+// Change the values from an id element using javascript instead of textContent we can use innerHTML
+//document.getElementById("score--0").innerHTML = 0;
+//document.getElementById("score--1").innerHTML = 0;
+
+
+
+// Event Listener for button ROLL DICE // What happens when you click on button ROLL DICE
+
+document.querySelector(".btn--roll").addEventListener('click',function(){
+
+        // What happens when you use the button "ROLL DICE"
+
+    if(gamePlaying)
+    {
+           // generate random number
+    dice = Math.floor(Math.random()*6+1); //generate a value for dice//
+
+    // display the result
+    var diceDOM =  document.querySelector(".dice");
+    diceDOM.style.display = 'block';
+    /*change src attribut using javascript*/ diceDOM.src = "dice-"+dice+".png";
+    
+    // Upgrade the round score IF the rolled number was not 1
+    if(dice !== 1)
+    {
+        
+       roundScore += dice; // update current score for the active player
+       document.querySelector("#current--"+activeplayer).innerHTML = roundScore;
+
+    }else{
+
+        roundScore = 0;
+        nextPlayer();
+    }
+
+    }
+
+
+    
+ 
+
+});
+
+
+// Event Listener for button HOLD
+
+document.querySelector(".btn--hold").addEventListener('click',function(){
+    
+    if(gamePlaying)
+    {
+    // Add currentScore to Global Score
+    scores[activeplayer] += roundScore;
+    // or more easily scores[activeplayer] += roundScore;
+    // Update UI
+    document.querySelector("#score--"+activeplayer).textContent = scores[activeplayer];
+
+    
+    //Check if the player won the game
+    if(scores[activeplayer] >= 20)
+    {
+        document.getElementById("name--"+activeplayer).textContent = "WINNER";
+        document.querySelector('.dice').style.display = 'none';
+        gamePlaying = false;
+    }
+        
+    // Next player
+    nextPlayer();
+
+    }
+});
+
+
+// Event Listener for button NEW GAME
+document.querySelector(".btn--new").addEventListener('click',initGame);
+
+
+
+
+
+
+
+
 
 
 // every time the  activeplayer switches the dice should not be displayed before the players rolls it
-document.querySelector(".dice").style.display = 'none';
+//document.querySelector(".dice").style.display = 'none';
+
+
